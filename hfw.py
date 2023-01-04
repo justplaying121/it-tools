cc2cb6bbb51e67bcc998ef24d3ffeddf0f9a510f
justplaying121 <121928925+justplaying121@users.noreply.github.com>
2023-01-04 13:31:49 +0530

    Add files via upload

---
+++ hfw.py
@@ -0,0 +1,13 @@
+class numbers:
+    x=int(input("enter anumber"))
+    y=int(input("enter a number"))
+def add(x,y):
+    sum = x + y
+def sub(x,y):
+    sum = x - y
+def div(x,y):
+    sum = x / y
+def multi(x,y):
+    sum = x * y
+def display(self):
+    print(self.num1,self.num2)
